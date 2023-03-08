d2578a3209510a50ac26451e6c2369d29925af68
Yusuke Imai <yusuke.imai.41@gmail.com>
2023-03-08 12:30:36 +0000

    Initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+    #質問一覧表示
+    def index
+        @questions = Question.all
+    end
+    
+    #質問詳細ページ表示
+    def show
+        @question = Question.find(params[:id])
+        p @question
+    end
+    
+    #質問の作成
+    def new
+        @question = Question.new
+    end
+
+    #質問の登録
+    def create
+        #Questionモデルを初期化
+        @question = Question.new(question_params)
+        
+        #QuestionモデルをDBへ保存
+        if @question.save
+            #showへリダイレクト
+            redirect_to @question
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    #質問の編集
+    def edit
+        @question = Question.find(params[:id])
+    end
+    
+    #質問の更新
+    def update
+        @question = Question.find(params[:id])
+        if @question.update(question_params)
+            redirect_to @question
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+    
+    #質問の削除
+    def destroy
+        @question = Question.find(params[:id])
+        @question.destroy
+        redirect_to questions_path
+    end
+    
+    private
+    
+    #ストロングパラメータの設定
+    def question_params
+        params.require(:question).permit(:title, :name, :content)
+    end
+
+end
